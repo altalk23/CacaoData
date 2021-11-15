1ee081e27aec7dd562032a44fc987d17c59962ee
Alkım Deniz Altıntaş <altalk.23@robcol.k12.tr>
2021-11-15 15:20:25 +0300

    2 files

--- Entry.mm
+++ Entry.mm
@@ -1,18 +1,2 @@
-#include <CocosEntry.mm>
-#include <Delegates.mm>
-#include <RobCocos.mm>
-#include <Managers.mm>
-
-#include <Layers.mm>
-#include <Scenes.mm>
-
-#include <BaseNodes.mm>
-#include <Components.mm>
-#include <GameObjects.mm>
-
-#include <InfoObjects.mm>
-#include <Cells.mm>
-#include <Actions.mm>
-#include <Sprites.mm>
-
+#include <Cocos2d.mm>
 #include <GeometryDash.mm>
